f9632e40babc7bc24d9d174fe52576b6b0f7a4a9
SND\MGholam_cp <SND\MGholam_cp@0ce29038-4a70-45ae-8a1e-7d2f2ce429ee>
2011-11-12 06:02:18 +0000

    - added missing files

---
+++ RaptorDB/DataTypes/DataTypes.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaptorDB
+{
+    public interface IRDBDataType<T> : IComparable<T>, IGetBytes<T>, IEqualityComparer<T>, IEquatable<T>
+    {
+    }
+    
+    //------------------------------------------------------------------------------------------------------------------
+
+    public interface IGetBytes<T>
+    {
+        byte[] GetBytes();
+        T GetObject(byte[] buffer, int offset, int count);
+    }
+
+    //----------------------------------------------------------------------------------------------------
+
+    public class rdbByteArray : IRDBDataType<rdbByteArray>
+    {
+        public rdbByteArray()
+        {
+
+        }
+
+        public rdbByteArray(byte[] key)
+        {
+            val = key;
+        }
+        public byte[] val;
+
+        public int CompareTo(rdbByteArray obj)
+        {
+            return Helper.CompareUnSafe(val, obj.val);
+        }
+
+        public byte[] GetBytes()
+        {
+            return val;
+        }
+
+        public bool Equals(rdbByteArray a, rdbByteArray b)
+        {
+            if (a == null || b == null)
+                return a == b;
+            if (a.val.Length != b.val.Length)
+                return false;
+            int i = Helper.CompareUnSafe(a.val, b.val);
+            return i==0?true:false;
+        }
+
+        public int GetHashCode(rdbByteArray obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException();
+            int iHash = 0;
+            for (int i = 0; i < obj.val.Length; ++i)
+                iHash ^= (obj.val[i] << ((0x03 & i) << 3));
+            return iHash;
+        }
+
+        public bool Equals(rdbByteArray other)
+        {
+            if (other == null)
+                return false;
+            if (this.val.Length != other.val.Length)
+                return false;
+            int i = Helper.CompareUnSafe(this.val, other.val);
+            return i == 0 ? true : false;
+        }
+
+        public rdbByteArray GetObject(byte[] buffer, int offset, int count)
+        {
+            byte[] b = new byte[count];
+            Buffer.BlockCopy(buffer, offset, b, 0, count);
+            return new rdbByteArray(b);
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------------
+
+    public class rdbInt : IRDBDataType<rdbInt>
+    {
+        public rdbInt()
+        {
+
+        }
+        public rdbInt(int i)
+        {
+            _i = i;
+        }
+        public rdbInt(uint i)
+        {
+            _i = (int)i;
+        }
+        private int _i;
+
+        public int CompareTo(rdbInt other)
+        {
+            return _i.CompareTo(other._i);
+        }
+
+        public byte[] GetBytes()
+        {
+            return Helper.GetBytes(_i, false);
+        }
+
+        public bool Equals(rdbInt x, rdbInt y)
+        {
+            return x._i == y._i;
+        }
+
+        public int GetHashCode(rdbInt obj)
+        {
+            return obj._i;
+        }
+
+        public bool Equals(rdbInt other)
+        {
+            return this._i == other._i;
+        }
+
+        public rdbInt GetObject(byte[] buffer, int offset, int count)
+        {
+            return new rdbInt(Helper.ToInt32(buffer, offset));
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------------
+
+    public class rdbLong : IRDBDataType<rdbLong>
+    {
+        public rdbLong()
+        {
+
+        }
+        public rdbLong(long i)
+        {
+            _i = i;
+        }
+        private long _i;
+
+        public int CompareTo(rdbLong other)
+        {
+            return _i.CompareTo(other._i);
+        }
+
+        public byte[] GetBytes()
+        {
+            return Helper.GetBytes(_i, false);
+        }
+
+        public bool Equals(rdbLong x, rdbLong y)
+        {
+            return x._i == y._i;
+        }
+
+        public int GetHashCode(rdbLong obj)
+        {
+            return obj._i.GetHashCode();
+        }
+
+        public bool Equals(rdbLong other)
+        {
+            return this._i == other._i;
+        }
+
+        public rdbLong GetObject(byte[] buffer, int offset, int count)
+        {
+            return new rdbLong(Helper.ToInt64(buffer, offset));
+        }
+    }
+}
---
+++ RaptorDB/RaptorDBString.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RaptorDB
+{
+    public class RaptorDBString : RaptorDBbase
+    {
+        private bool _caseSensitive = false;
+
+        public RaptorDBString(string filename, bool caseSensitive)
+            : base(filename)
+        {
+            _caseSensitive = caseSensitive;
+        }
+
+        public void Set(string key, string val)
+        {
+            this.Set(key, Encoding.Unicode.GetBytes(val));
+        }
+
+        public void Set(string key, byte[] val)
+        {
+            string str = (_caseSensitive ? key : key.ToLower());
+            byte[] ustr = Encoding.Unicode.GetBytes(str);
+
+            base.Set(ustr, val);
+        }
+
+        public bool Get(string key, out string val)
+        {
+            byte[] bval = null;
+            val = null;
+
+            if (this.Get(key, out bval))
+            {
+                val = Encoding.Unicode.GetString(bval);
+                return true;
+            }
+            return false;
+        }
+
+        public bool Get(string key, out byte[] val)
+        {
+            string str = (_caseSensitive ? key : key.ToLower());
+            byte[] ustr = Encoding.Unicode.GetBytes(str);
+
+            return base.Get(ustr, out val);
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------------
+
+    public class RaptorDBGuid : RaptorDBbase
+    {
+        public RaptorDBGuid(string filename)
+            : base(filename)
+        {
+        }
+
+        public void Set(Guid key, string val)
+        {
+            this.Set(key, Encoding.Unicode.GetBytes(val));
+        }
+
+        public void Set(Guid key, byte[] val)
+        {
+            base.Set(key.ToByteArray(), val);
+        }
+
+        public bool Get(Guid key, out string val)
+        {
+            byte[] bval = null;
+            val = null;
+
+            if (this.Get(key.ToByteArray(), out bval))
+            {
+                val = Encoding.Unicode.GetString(bval);
+                return true;
+            }
+            return false;
+        }
+
+        public bool Get(Guid key, out byte[] val)
+        {
+            return base.Get(key.ToByteArray(), out val);
+        }
+
+        public IEnumerable<int> GetDuplicates(Guid key)
+        {
+            return base.GetDuplicates(key.ToByteArray());
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------------
+
+    public abstract class RaptorDBbase
+    {
+        internal RaptorDB<rdbInt> _rap;
+        private MurmurHash2Unsafe _mur = new MurmurHash2Unsafe();
+
+        public RaptorDBbase(string filename)
+        {
+            Global.DEFAULTNODESIZE = 1000;
+            _rap = new RaptorDB<rdbInt>(filename, 4, true, INDEXTYPE.BTREE, false);
+            _rap.InMemoryIndex = true;
+            _rap.StartIndexerThread();
+        }
+
+
+        public void Set(byte[] key, byte[] val)
+        {
+            uint hc = _mur.Hash(key);
+
+            MemoryStream ms = new MemoryStream();
+            ms.Write(Helper.GetBytes(key.Length, false), 0, 4);
+            ms.Write(key, 0, key.Length);
+            ms.Write(val, 0, val.Length);
+
+            _rap.Set(new rdbInt(hc), ms.ToArray());
+        }
+
+        public bool Get(byte[] key, out byte[] val)
+        {
+            uint hc = _mur.Hash(key);
+            //byte[] k = Helper.GetBytes((int)hc, false);
+
+            if (_rap.Get(new rdbInt(hc), out val))
+            {
+                // unpack data
+                byte[] g = null;
+                if (UnpackData(val, out val, out g))
+                {
+                    if (Helper.CompareMemCmp(key, g) != 0)
+                    {
+                        // if data not equal check duplicates (hash conflict)
+                        List<int> ints = new List<int>(_rap.GetDuplicates(new rdbInt((int)hc)));
+                        ints.Reverse();
+                        foreach (int i in ints)
+                        {
+                            byte[] bb = _rap.FetchDuplicate(i);
+                            if (UnpackData(bb, out val, out g))
+                            {
+                                if (Helper.CompareMemCmp(key, g) == 0)
+                                    return true;
+                            }
+                        }
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Shutdown()
+        {
+            _rap.Shutdown();
+        }
+
+        public void SaveIndex()
+        {
+            _rap.SaveIndex();
+        }
+
+        public long Count()
+        {
+            return _rap.Count();
+        }
+
+        private bool UnpackData(byte[] buffer, out byte[] val, out byte[] key)
+        {
+            int len = Helper.ToInt32(buffer, 0, false);
+            key = new byte[len];
+            Buffer.BlockCopy(buffer, 4, key, 0, len);
+            val = new byte[buffer.Length - 4 - len];
+            Buffer.BlockCopy(buffer, 4 + len, val, 0, buffer.Length - 4 - len);
+
+            return true;
+        }
+
+        public IEnumerable<int> GetDuplicates(byte[] key)
+        {
+            uint hc = _mur.Hash(key);
+
+            return _rap.GetDuplicates(new rdbInt(hc));
+        }
+
+        public byte[] FetchDuplicate(int rec)
+        {
+            byte[] b = _rap.FetchDuplicate(rec);
+            byte[] val;
+            byte[] key;
+            if (UnpackData(b, out val, out key))
+            {
+                return val;
+            }
+            return null;
+        }
+    }
+}
